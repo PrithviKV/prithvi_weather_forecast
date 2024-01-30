93627480eadd93a5934d30591f1e6284999cccbd
PrithviKV <kv.prithvi5@gmail.com>
2024-01-29 16:16:38 -0800

    add unit test - address service

---
+++ app/services/address_service.rb
@@ -0,0 +1,46 @@
+class AddressService
+
+    attr_accessor :address
+
+    def initialize(address)
+        @address = address
+        @street = ""
+        @city = ""
+        @state = ""
+        @zip = ""
+        @url = ""
+    end
+
+    #prepare the url from the address
+    def get_url
+
+        address_hash = get_address_hash
+        @street = address_hash["street"]
+        @city = address_hash["city"]
+        @state = address_hash["state"]
+        @zipcode = address_hash["zipcode"]
+
+        @url = "https://geocoding.geo.census.gov/geocoder/locations/address?street=#{@street}&city=#{@city}&state=#{@state}&zip=#{@zipcode}&benchmark=2020&format=json"
+
+        @url
+    end
+
+    def get_address_hash
+        
+        arr = @address.split(/,/)
+        street = arr[0].split.join('+')
+        city = arr[1].strip
+        state = arr[2].split[0]
+        zip = arr[2].split[1]
+
+        return { "street" => street, "city" => city, "state" => state, "zipcode" => zip }
+    end
+
+    #regex to check address is in the from 4241354 Street name, city name, state code zipcode"
+    def is_correct_format?
+
+        return @address.match? /\A[0-9]*\s+[a-zA-Z]+\s+[a-zA-Z]+[\s]*[a-zA-Z]*[\s]*[,][\s]*[a-zA-Z]+[\s]*[,][\s]*[A-Z][A-Z]\s+\d+[\s]*\Z/
+
+    end
+end
+
---
+++ test/services/address_service_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class AddressServiceTest < ActiveSupport::TestCase
+    test "call with valid address" do
+        address = "454 Bolinger Cmn, Fremont, CA 94539"
+        address_service = AddressService.new(address)
+        assert_equal "https://geocoding.geo.census.gov/geocoder/locations/address?street=454+Bolinger+Cmn&city=Fremont&state=CA&zip=94539&benchmark=2020&format=json", address_service.get_url
+        
+        address_hash = address_service.get_address_hash
+        assert_equal "454+Bolinger+Cmn", address_hash["street"]
+        assert_equal "Fremont", address_hash["city"]
+        assert_equal "CA", address_hash["state"]
+        assert_equal "94539", address_hash["zipcode"]
+        
+        assert_equal true, address_service.is_correct_format?
+    end
+
+    test "call with invalid address" do
+        address = "Bolinger Cmn, Fremont, CA 94539"
+        address_service = AddressService.new(address)
+        
+        assert_equal false, address_service.is_correct_format?
+    end
+
+end
