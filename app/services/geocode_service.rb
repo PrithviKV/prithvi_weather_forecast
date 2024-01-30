faef08e26ec0959d71482626b0204c7cb10527ac
PrithviKV <kv.prithvi5@gmail.com>
2024-01-29 16:16:07 -0800

    add unit test - geocode service

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -7,63 +7,30 @@ class GeocodeService
 
     def initialize(address)
         @address = address
-    end
-
-    def get_url
-        
-        #prepare the url from the address
-
-        url = ""
-
-        begin
-            address_hash = get_address_hash
-            @street = address_hash["street"]
-            @city = address_hash["city"]
-            @state = address_hash["state"]
-            @zipcode = address_hash["zipcode"]
-
-            url = "https://geocoding.geo.census.gov/geocoder/locations/address?street=#{@street}&city=#{@city}&state=#{@state}&zip=#{@zipcode}&benchmark=2020&format=json"
-
-        rescue => e
-            return e.message
-        end
-
-        url
-    end
-
-    def get_address_hash
-
-        arr=@address.split(/,/)
-        street=arr[0].split.join('+')
-        city=arr[1].strip
-        state=arr[2].split[0]
-        zip = arr[2].split[1]
-        return { "street" => street, "city" => city, "state" => state, "zipcode" => zip }
-
+        @longitude = 0
+        @latitude = 0
     end
 
     def get_coordinates
 
-        url = get_url
+        url = AddressService.new(@address).get_url
 
-        return if !url
+        if url
 
-        longitude = 0
-        latitude = 0
-
-        begin
-            uri = URI(url)
-            res = Net::HTTP.get_response(uri)
-            body = JSON.parse(res.body)
-            
-            if !body["result"]["addressMatches"].empty?
-                longitude=body["result"]["addressMatches"][0]["coordinates"]["x"].round(4)
-                latitude=body["result"]["addressMatches"][0]["coordinates"]["y"].round(4)
+            begin
+                uri = URI(url)
+                res = Net::HTTP.get_response(uri)
+                body = JSON.parse(res.body)
+                
+                if !body["result"]["addressMatches"].empty?
+                    @longitude=body["result"]["addressMatches"][0]["coordinates"]["x"].round(4)
+                    @latitude=body["result"]["addressMatches"][0]["coordinates"]["y"].round(4)
+                end
+            rescue => e
+                return e.message
             end
-        rescue => e
-            return e.message
         end
 
-        [latitude, longitude]
+        [@latitude, @longitude]
     end
 end
--- test/services/geocode_service_test.rb
+++ test/services/geocode_service_test.rb
@@ -4,23 +4,20 @@ class GeocodeServiceTest < ActiveSupport::TestCase
     test "call with valid address" do
         address = "454 Bolinger Cmn, Fremont, CA 94539"
         geocode = GeocodeService.new(address)
-        assert_equal "https://geocoding.geo.census.gov/geocoder/locations/address?street=454+Bolinger+Cmn&city=Fremont&state=CA&zip=94539&benchmark=2020&format=json", geocode.get_url
-        
-        address_hash = geocode.get_address_hash
-        assert_equal "454+Bolinger+Cmn", address_hash["street"]
-        assert_equal "Fremont", address_hash["city"]
-        assert_equal "CA", address_hash["state"]
-        assert_equal "94539", address_hash["zipcode"]
         
         coordinates = geocode.get_coordinates
         assert_in_delta -121.92259, coordinates[1], 0.1
         assert_in_delta 37.48664, coordinates[0], 0.1
     end
 
-    # test "call with invalid address" do
-    #     address = "454 Bolinger Fremont, CA 94539"
-    #     geocode = GeocodeService.new(address)
+    test "call with invalid address" do
+        address = "4 Br Cmn, Fr, CA 9"
+        geocode = GeocodeService.new(address)
+        
+        coordinates = geocode.get_coordinates
+        assert_in_delta 0, coordinates[1], 0.1
+        assert_in_delta 0, coordinates[0], 0.1
 
-    #     assert_equal "", geocode.get_url
-    # end
+    end
 end
+
