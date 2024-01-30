b3bbbfaa6e21ef918cd0499c6f121f2431a24f9b
PrithviKV <kv.prithvi5@gmail.com>
2024-01-29 16:17:33 -0800

    code refactoring

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -3,24 +3,30 @@ class ForecastsController < ApplicationController
 
         @address = params[:address]
         
+        address_service = AddressService.new(@address)
+
         #check address is present and it is in the right format
-        if @address && is_correct_format?
+        if @address && address_service.is_correct_format?
             begin
-                
-                @geocode = GeocodeService.new(@address)
 
                 # zipcode as weather_cache_key
-                @weather_cache_key = @geocode.get_address_hash["zipcode"]
+                @weather_cache_key = address_service.get_address_hash["zipcode"]
+                
 
                 @weather_cache_exist = Rails.cache.exist?(@weather_cache_key)
 
                 @data = Rails.cache.fetch(@weather_cache_key, expires_in: 30.minutes) do
 
+                    @geocode = GeocodeService.new(@address)
+
                     #get longitude and latitude from the address
                     latitude, longitude = @geocode.get_coordinates
 
-                    #get forecast data from Api
-                    WeatherService.call(latitude, longitude)
+                    if latitude != 0 && longitude != 0
+
+                        #get forecast data from Api
+                        WeatherService.call(latitude, longitude)
+                    end
                 end
 
             rescue => e
@@ -31,15 +37,6 @@ class ForecastsController < ApplicationController
         else
             flash.alert = "Please enter a valid address in the form \“4600 Silver Hill Rd, Washington, DC 20233\”"
         end
-    end
-
-    private
-    
-    #regex to check address is in the from 4241354 Street name, city name, state code zipcode"
-    def is_correct_format?
-
-        return @address.match? /\A\d+\s+[a-zA-Z]+\s+[a-zA-Z]+[\s]*[a-zA-Z]*[\s]*[,][\s]*[a-zA-Z]+[\s]*[,][\s]*[A-Z][A-Z]\s+\d+[\s]*\Z/
-
     end
 end
 
